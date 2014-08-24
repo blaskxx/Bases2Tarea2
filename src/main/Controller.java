75027db3a8b6b659aaf33c6e1c0d2b0a85e66670
josanuz <josanuz@hotmail.es>
2014-08-24 13:53:15 -0600

    Agregado Primeros Controles para Tabla

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -1,13 +1,43 @@
 package main;
 
+import edu.una.adb.entities.Init_File;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.util.Callback;
+
 
 import java.net.URL;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+    @FXML
+    TableView t_file_props;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        Init_File ini = new Init_File();
+        Map<String, String> map = ini.getData();
 
+
+        // use fully detailed type for Map.Entry<String, String>
+        TableColumn<Map.Entry<String, String>, String> column1 = new TableColumn<>("Opción");
+        column1.setCellValueFactory((p)-> new SimpleStringProperty(p.getValue().getKey()));
+
+        TableColumn<Map.Entry<String, String>, String> column2 = new TableColumn<>("Valor");
+        column2.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().getValue()));
+
+        ObservableList<Map.Entry<String, String>> items = FXCollections.observableArrayList(map.entrySet());
+        t_file_props.setItems(items);
+        column2.setEditable(true);
+        column1.setEditable(true);
+        t_file_props.setEditable(true);
+
+        t_file_props.getColumns().setAll(column1, column2);
     }
 }
