ed7f040b6e3ba73ca7b5c113405dc5482a24cf3e
josanuz <josanuz@hotmail.es>
2014-08-24 17:22:26 -0600

    Tabla Editable Terminada

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -5,39 +5,64 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.input.MouseEvent;
 import javafx.util.Callback;
-
 
 import java.net.URL;
 import java.util.Map;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
-    @FXML
-    TableView t_file_props;
+    Map<String, String> map;
+    @FXML private TableView t_file_props;
+    @FXML private Button BT_add;
+    @FXML private TextField TF_opc;
+    @FXML private TextField TF_val;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         Init_File ini = new Init_File();
-        Map<String, String> map = ini.getData();
+        map = ini.getData();
 
-
+        //**************************************************************************************************//
+        //Table
         // use fully detailed type for Map.Entry<String, String>
         TableColumn<Map.Entry<String, String>, String> column1 = new TableColumn<>("Opción");
         column1.setCellValueFactory((p)-> new SimpleStringProperty(p.getValue().getKey()));
 
+
         TableColumn<Map.Entry<String, String>, String> column2 = new TableColumn<>("Valor");
         column2.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().getValue()));
+        column2.setCellFactory(TextFieldTableCell.forTableColumn());
+        column2.setOnEditCommit(e ->{
+            Map.Entry<String,String> entry = e.getTableView().getItems().get(e.getTablePosition().getRow());
+            map.put(entry.getKey(),e.getNewValue());
+        });
 
         ObservableList<Map.Entry<String, String>> items = FXCollections.observableArrayList(map.entrySet());
-        t_file_props.setItems(items);
+
         column2.setEditable(true);
-        column1.setEditable(true);
+        column1.setEditable(false);
         t_file_props.setEditable(true);
 
+        t_file_props.setItems(items);
         t_file_props.getColumns().setAll(column1, column2);
+
+        /***                    BUTTONS                     ***/
+        /*--------------------------------------------------**/
+        BT_add.setOnMousePressed(e->{
+            if(TF_opc.getText()==null || TF_val.getText() == null) return;
+            if(TF_opc.getText().trim().equals("") || TF_val.getText().trim().equals("")) return;
+            map.put(TF_opc.getText(),TF_val.getText());
+            items.clear();items.addAll(map.entrySet());
+        });
+
     }
+
 }
